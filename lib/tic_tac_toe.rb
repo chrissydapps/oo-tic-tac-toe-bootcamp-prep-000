63f6b4306ac23d1a1335725a9377cc31e286212d
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-20 17:00:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -80,8 +80,8 @@ class TicTacToe
     @board.all? {|token| token == "X"||token == "O"}
   end
 
-  def draw?(board)
-    !won?(board) && full?(board)
+  def draw?
+    !won? && full?
   end
 
   def over?(board)
