dc991d2fafa564894b331ff7500bed6b433adefe
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-20 16:55:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,13 +50,14 @@ class TicTacToe
   end
 
   def turn_count(board)
-    number_of_turns = 0
-    board.each do |cell|
-      if cell == "X" || cell == "O"
-        number_of_turns += 1
-      end
-    end
-    number_of_turns
+    @board.count{|token| token == "X" || token == "O"}
+    # number_of_turns = 0
+    # board.each do |cell|
+    #   if cell == "X" || cell == "O"
+    #     number_of_turns += 1
+    #   end
+    # end
+    # number_of_turns
   end
 
   def current_player(board)
